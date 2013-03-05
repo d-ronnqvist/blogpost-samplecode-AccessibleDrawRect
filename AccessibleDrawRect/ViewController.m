6dfe7e3801a93d262a2a74327267a2acac7a8def
David Rönnqvist <david.ronnqvist@gmail.com>
2013-03-05 07:45:03 +0100

    Initial Commit

---
+++ AccessibleDrawRect/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AccessibleDrawRect
+//
+//  Created by David Rönnqvist on 3/5/13.
+//  Copyright (c) 2013 David Rönnqvist. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AccessibleDrawRect/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AccessibleDrawRect
+//
+//  Created by David Rönnqvist on 3/5/13.
+//  Copyright (c) 2013 David Rönnqvist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
